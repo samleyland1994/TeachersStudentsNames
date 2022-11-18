13360b83d9287630fae6ff0e417c1e1720dcf20c
samleyland1994 <samleyland1994@gmail.com>
2022-11-18 14:13:28 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("How many students are in your class?");
+Console.ReadLine();
+string[] studentNames = new string[6];
+Console.WriteLine("Please input names");
+for (int i = 0; i < studentNames.Length; i++)
+{
+    studentNames[i] = Console.ReadLine();
+}
+Console.WriteLine("\nHere they are alphabetically");
+
+Array.Sort(studentNames);
+
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine(studentNames[i]);
+}
+Console.ReadKey();
+
+
+
